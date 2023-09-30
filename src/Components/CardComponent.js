8c9e5d7b1324c0ee0715a2f8f5345311db4e76c0
ateeshsingh <ateeshchauhan4023@gmail.com>
2023-09-30 16:11:19 +0530

    change cardComponent

--- src/Components/CardComponent.js
+++ src/Components/CardComponent.js
@@ -1,6 +1,6 @@
 import { Card } from "antd";
 import { useState } from 'react';
-import DrawerComponent from "./DrawerComponent";
+import DrawerComponent from "../components/DrawerComponent";
 
 function CardComponent (props){
     const {data}=props
